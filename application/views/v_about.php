ae3928f099fde8a1180c580f40bee738fcbea778
LoneWolf12345 <muhamadsandi2105@gmail.com>
2022-04-02 17:40:40 +0700

    menambahkan contoh 1,2 dan 3 yaitu = latihan1.php di dalam application/controllers,model_latihan1.php di application/model,view-latihan.php di dalam application/views

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Sandi</li>
+        <li>Nama Belakang : Muhamad</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl kapuk Santri Rt12/Rw12 </li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta Barat</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
